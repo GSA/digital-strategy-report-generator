7c258423d87552dafabe0bade224e3e77a310a09
Benjamin J. Balter <ben@balter.com>
2012-08-13 09:49:46 -0400

    simplify generate.php; use the php system temp directory for better WAMP support

--- includes/generate.php
+++ includes/generate.php
@@ -50,35 +50,29 @@ foreach ( $dgs_items as &$item ) {
 
 }
 
-//create temporary scratch directory
-$dir['tmp'] = '/tmp/' . md5( time() );
-
-// Check for a value in the report directory constant.
-if (DGS_REPORT_DIR) {
-  // Files will be created in the specified report directory and will not be zipped.
-  $dir['rpt'] = DGS_REPORT_DIR;
-}
 // Files will be created in a temporary directory and added to zip file.
-// Create the temporary directory.
-mkdir( $dir['tmp'] );
+
+//create temporary scratch directory in PHP's default temp directory
+//this allows the project to run on both unix and WAMP systems (e.g., where /tmp/ doesn't exist)
+$dir = sys_get_temp_dir() . '/dgs-' . md5( time() );
+mkdir( $dir );
+
+//generate the payload
+
+//json
+file_put_contents( $dir . '/digitalstrategy.json', json_encode( $report ) );
+
+//xml
+$xml = new SimpleXMLElement( '<report></report>' );
+$xml = dgs_to_xml( $report, $xml );
+file_put_contents(  $dir . '/digitalstrategy.xml', dgs_tidy_xml( $xml ) );
+
+//html
+file_put_contents( $dir . '/digitalstrategy.html', dgs_to_html( $report ) );
+
 // Create zip file.
-$zipfile = "{$report->agency}-report.zip";
-
-foreach ($dir as $id => $loc) {
-  //json
-  file_put_contents( $loc . '/digitalstrategy.json', json_encode( $report ) );
-
-  //xml
-  $xml = new SimpleXMLElement( '<report></report>' );
-  $xml = dgs_to_xml( $report, $xml );
-  file_put_contents(  $loc . '/digitalstrategy.xml', dgs_tidy_xml( $xml ) );
-
-  //html
-  file_put_contents( $loc . '/digitalstrategy.html', dgs_to_html( $report ) );
-}
-
-$zip = "{$dir['tmp']}/$zipfile";
-dgs_zip( $dir['tmp'], $zip );
+$filename = "{$report->agency}-report.zip";
+dgs_zip( $dir, "{$dir}/$filename" );
 
 //send headers
 header( 'Content-Type: application/zip' );
@@ -88,7 +82,12 @@ header( 'Content-Length: ' . filesize( $zip ) );
 //read file
 readfile( $zip );
 
+//Check for a value in the report directory constant
+//if it exists, move the files into the report directory for easy access before clearing the scratch directory
+if ( DGS_REPORT_DIR )
+	copy( $dir, DGS_REPORT_DIR );
+
 //cleanup
-dgs_cleanup( $dir['tmp'] );
+dgs_cleanup( $dir );
 
 exit();
